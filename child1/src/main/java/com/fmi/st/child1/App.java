eb0d46b1671390e438b50205ec3352beca27fa8d
shefikamustafa <50406914+shefikamustafa@users.noreply.github.com>
2019-05-21 12:32:40 +0300

    clash free time table

---
+++ child1/src/main/java/com/fmi/st/child1/App.java
@@ -0,0 +1,13 @@
+package com.fmi.st.child1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Pesho12!" );
+    }
+}
